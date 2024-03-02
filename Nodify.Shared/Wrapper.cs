3a72458fca02064c12f1f47a25f11416c92993cf
eocron <ad-company@mail.ru>
2024-03-02 04:32:30 +0300

    Added vector constant

---
+++ Nodify.Shared/Wrapper.cs
@@ -0,0 +1,25 @@
+﻿namespace Nodify.Shared
+{
+    public class Wrapper<T>
+    {
+        public T Item { get; set; }
+
+        public Wrapper(T value = default(T))
+        {
+            Item = value;
+        }
+
+        public static implicit operator Wrapper<T>(T item)
+        {
+            return new Wrapper<T>(item);
+        }
+
+        public static implicit operator T(Wrapper<T> item)
+        {
+            if (null != item)
+                return item.Item;
+
+            return default(T);
+        }
+    }
+}
--- ProceduralWorldGenerator/ViewModels/Nodes/VectorNodeViewModel.cs
+++ ProceduralWorldGenerator/ViewModels/Nodes/VectorNodeViewModel.cs
@@ -1,4 +1,6 @@
-﻿using ProceduralWorldGenerator.Validation;
+﻿using System.Collections.ObjectModel;
+using Nodify.Shared;
+using ProceduralWorldGenerator.Validation;
 using ProceduralWorldGenerator.ViewModels.Nodes.Parameters;
 
 namespace ProceduralWorldGenerator.ViewModels.Nodes
@@ -9,6 +11,8 @@ namespace ProceduralWorldGenerator.ViewModels.Nodes
         {
             Title = "v"
         };
+
+        private bool _isConstant;
 
         public VectorParameterViewModel Value
         {
@@ -29,10 +33,33 @@ namespace ProceduralWorldGenerator.ViewModels.Nodes
             get => Value.Dimension;
             set
             {
+                var diff = Values.Count - value;
+                if (diff > 0)
+                {
+                    for (int i = 0; i < diff; i++)
+                    {
+                        Values.RemoveAt(Values.Count-1);
+                    }
+                }
+                else
+                {
+                    for (int i = 0; i < -diff; i++)
+                    {
+                        Values.Add(0);
+                    }
+                }
                 SetNestedProperty(nameof(Value), Value.Dimension, value, () => Value.Dimension = value);
             }
         }
-        
+
+        public bool IsConstant
+        {
+            get => _isConstant;
+            set => SetProperty(ref _isConstant, value);
+        }
+
+        public ObservableCollection<Wrapper<float>> Values { get; set; } = new ObservableCollection<Wrapper<float>>();
+
         public override string Title => Dimension != 0 ? $"{VariableName} {Dimension}D" : VariableName;
     }
 }
