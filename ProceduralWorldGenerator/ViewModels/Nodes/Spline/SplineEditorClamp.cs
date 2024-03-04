f8ebd5597b995061eeb8a8fac503967ece9946cd
eocron <ad-company@mail.ru>
2024-03-05 01:30:40 +0300

    Added description to clamp

--- ProceduralWorldGenerator/ViewModels/Nodes/Spline/SplineEditorClamp.cs
+++ ProceduralWorldGenerator/ViewModels/Nodes/Spline/SplineEditorClamp.cs
@@ -1,9 +1,14 @@
-﻿namespace ProceduralWorldGenerator.ViewModels.Nodes.Spline
+﻿using System.ComponentModel;
+
+namespace ProceduralWorldGenerator.ViewModels.Nodes.Spline
 {
     public enum SplineEditorClamp
     {
+        [Description("Use edge value")]
         LastValue,
+        [Description("Ping-Pong loop")]
         PingPong,
+        [Description("Loop")]
         Loop
     }
 }
