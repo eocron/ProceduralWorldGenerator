81540048fe79965c1cb682cd0bc5c04791300956
eocron <ad-company@mail.ru>
2024-03-05 01:26:01 +0300

    Implemented right/left clamp on splines

--- ProceduralWorldGenerator/ViewModels/Nodes/Spline/SplineEditorViewModel.cs
+++ ProceduralWorldGenerator/ViewModels/Nodes/Spline/SplineEditorViewModel.cs
@@ -6,8 +6,21 @@ namespace ProceduralWorldGenerator.ViewModels.Nodes.Spline
 {
     public class SplineEditorViewModel : ObservableObject
     {
-        public SplineEditorClamp LeftClamp { get; set; }
-        public SplineEditorClamp RightClamp { get; set; }
+        private SplineEditorClamp _leftClamp;
+        private SplineEditorClamp _rightClamp;
+
+        public SplineEditorClamp LeftClamp
+        {
+            get => _leftClamp;
+            set => SetProperty(ref _leftClamp, value);
+        }
+
+        public SplineEditorClamp RightClamp
+        {
+            get => _rightClamp;
+            set => SetProperty(ref _rightClamp, value);
+        }
+
         public BindingList<EditablePointViewModel> DataPoints { get; set; } = new BindingList<EditablePointViewModel>();
     }
 }
